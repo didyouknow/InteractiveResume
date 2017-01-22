58e2768bf28aca647a9384cb990cc61cb68446c5
didyouknow <didyouknowitsme@gmail.com>
2017-01-22 05:26:51 -0700

    Add files via upload

---
+++ AskMeAboutMyself.java
@@ -0,0 +1,14 @@
+package resume;
+
+public class AskMeAboutMyself {
+
+	public static void main(String[] args) {
+		// Make an interactive, futuristic resume so the employer can ask it questions
+
+		String askMe = "Please ask me a question. You can say" + " 'all' " + 
+		"for a list of all the current commands";
+		
+		System.out.print(askMe);
+	}
+
+}
