3376b19d200987c410b0a3153e7ecc8fb6234a4b
didyouknow <didyouknowitsme@gmail.com>
2017-01-23 03:06:38 -0700

    Update AskMeAboutMyself.java

--- AskMeAboutMyself.java
+++ AskMeAboutMyself.java
@@ -1,4 +1,4 @@
-package resume;
+import java.util.Scanner;
 
 public class AskMeAboutMyself {
 
@@ -7,8 +7,37 @@ public class AskMeAboutMyself {
 
 		String askMe = "Please ask me a question. You can say" + " 'all' " + 
 		"for a list of all the current commands";
+
+		System.out.println(askMe);
 		
-		System.out.print(askMe);
+		// Create scanner to recognize the answer
+		String answer = new Scanner(System.in).nextLine(); //Input to String
+		
+		//int result = Integer.parseInt(answer);
+		System.out.println("You said " + answer);
+		
+		String all = "All";
+		String capAll = "all";
+		//int compAll = Integer.parseInt(all);
+		//System.out.println(all);
+		
+		if (answer.equals(all)||answer.equals(capAll))
+			System.out.print("okay..."
+					+ "Name, Test2, Test3");
+		
+		String userAnswer = new Scanner(System.in).nextLine(); //Input to String
+		
+		//int result = Integer.parseInt(answer);
+		System.out.println("You said " + userAnswer);
+		
+		String name = "My name is Garry Moyer";
+		String qName = name;
+		switch (userAnswer){
+		case "Name": System.out.println(qName);
+		}
+		
+		
+		
 	}
 
 }
